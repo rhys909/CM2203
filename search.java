3834da024e2a7ccb41dd823f2d17c94c7c9f5ddf
rhys909 <rhyskconnor99@gmail.com>
2020-05-05 17:48:39 +0100

    Co-authored-by: Sam Jordan  <samjordan427@gmail.com>

--- search.java
+++ search.java
@@ -4,4 +4,12 @@ public class search {
     public void userSearch(String input){
         System.out.println(input);
     }
-}
+    //create methods that take user input
+    //add checks to ensure only 2000's music is output
+    // one Genres "Rock" 
+     //https://learningcentral.cf.ac.uk/bbcswebdav/pid-5337997-dt-content-rid-14468315_2/courses/1920-CM2203/CM2203%20Coursework%20Instructions%20and%20Marking%281%29.pdf
+}   //dictionary(artists:dkjdjdjdjd, albums:ahshdshh, years:2002,)
+
+
+// Artist dict("Britney Spears": [Pop, 0], "Jay Z": )
+// Albums dict("Toxic": [2002, 0], )
--- searchEngine.java
+++ searchEngine.java
@@ -1,30 +1,57 @@
 import javafx.application.*;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class searchEngine extends Application{
 
     @Override
     public void start(Stage primaryStage){
-        Textbox input = new Textbox();
+
+        //set the title of the page
+        Text scenetitle = new Text("Noughties Music \nSearch Engine");
+
+        //Initiate the text field and button to submit query
+        TextField input = new TextField();
+        //add radio buttons one for XML or JSON
         Button btn = new Button();
+
+        //set text of the button and add the event handler
         btn.setText("Search");
         btn.setOnAction(new EventHandler<ActionEvent>(){
         
             @Override
             public void handle(ActionEvent event) {
+                //search searching = new search();
                 //Add code here to take user input and perform the search (search class)
+                //searching.userSearch(input.getText());
+                System.out.println(input.getText());
+                //initiate the search object
+
             }
         });
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        //initiate the layout
+        GridPane root = new GridPane();
+
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+        root.setPadding(new Insets(25, 25, 25, 25));
+
+        //add textbox and buttons to the layout
+        root.add(scenetitle, 0, 0);
+        root.add(input, 0, 1);
+        root.add(btn, 0, 3);
         
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 300, 275);
 
         primaryStage.setTitle("2000's Music Search Engine");
         primaryStage.setScene(scene);
