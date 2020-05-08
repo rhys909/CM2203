7d434ba6bfbd91fa2e6fb302d37f77ccf63495f3
Sam <samjordan427@gmail.com>
2020-05-08 10:33:41 +0100

    few changes to dictionary

--- dictionary.java
+++ dictionary.java
@@ -3,12 +3,13 @@ import java.util.Hashtable;
 public class dictionary {
     public static void main(String[] args) {
 
-        Hashtable<String, String[]> artistDict = new Hashtable<String, String[]>();
-        Hashtable<String, String> albumsDict = new Hashtable<String, String>();
+        Hashtable<String, String> artistDict = new Hashtable<String, String>();
+        Hashtable<String, String[]> albumsDict = new Hashtable<String, String[]>();
         Hashtable<String, String[]> genreDict = new Hashtable<String, String[]>();
         
-        String[] outkastAlbums = {"Stankonia", "Speakerboxxx", "Idlewild"};
-        artistDict.put("OutKast", outkastAlbums);
+        //String[] outkastAlbums = {"Stankonia", "Speakerboxxx", "Idlewild"};
+        artistDict.put("OutKast", albumsDict.get("outkastAlbums"));
+        albumsDict.put("outkastAlbums", ["Stankonia" + "Speakerboxxx" + "Idlewild"]);
 
         System.out.println(artistDict.get("OutKast"));
 
