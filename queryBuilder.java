8c5006f3199c99a561e5c6e927b63b00662f0131
rhys909 <rhyskconnor99@gmail.com>
2020-05-08 12:26:55 +0100

    Fixed dictionary
    Co-authored-by: Sam Jordan  <samjordan427@gmail.com>

--- queryBuilder.java
+++ queryBuilder.java
@@ -1,16 +1,20 @@
-import java.sql.ResultSet;
-
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.ResourceFactory;
 
 class queryBuilder{
-    public static void main(String[] args){
+
+
+    public String query(String input){
         ParameterizedSparqlString qs = new ParameterizedSparqlString(""
                 + "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n"
                 + "PREFIX dbo:     <http://dbpedia.org/ontology/>"
                 + "\n"
-                + "select distinct ?resource ?abstract where {\n"
+                + "select distinct ?performer ?abstract where {\n"
                 + "  ?resource rdfs:label 'Ibuprofen'@en.\n"
                 + "  ?resource dbo:abstract ?abstract.\n"
                 + "  FILTER (lang(?abstract) = 'en')}");
@@ -18,10 +22,25 @@ class queryBuilder{
         QueryExecution exec = QueryExecutionFactory.sparqlService("http://dbpedia.org/sparql", qs.asQuery());
 
         ResultSet results = exec.execSelect();
+        String output = new String();
 
         while (results.hasNext()) {
 
-            System.out.println(results.next().get("abstract").toString());
+            output += results.next().get("abstract").toString();
+
         }
+        
+        return output;
     }
+
+    public String toXML(){
+        String XML = new String();
+        return XML;
+    }
+
+    public String toHTML(){
+        String HTML = new String();
+        return HTML;
+    }
+    
 }
