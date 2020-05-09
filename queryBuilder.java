b37d255a0b20cfa3fea1d25524ed3dcdae2d09c9
rhys909 <rhyskconnor99@gmail.com>
2020-05-09 14:09:46 +0100

    Added XMLtoJSON
    Co-authored-by: Sam Jordan  <samjordan427@gmail.com>

--- queryBuilder.java
+++ queryBuilder.java
@@ -1,4 +1,6 @@
 
+import org.apache.jena.JenaRuntime;
+import org.apache.jena.atlas.json.JsonString;
 import org.apache.jena.query.ParameterizedSparqlString;
 import org.apache.jena.query.Query;
 import org.apache.jena.query.QueryExecution;
@@ -8,46 +10,45 @@ import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.sparql.engine.http.QueryEngineHTTP;
+import org.json.*;
 
 
 class queryBuilder{
 
-    public static String query(){
-        ParameterizedSparqlString qs = new ParameterizedSparqlString();
+    public static String queryAlbumXML(){
+        ParameterizedSparqlString qs = new ParameterizedSparqlString(""
         //this builds the query template
-        qs.append("PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>");
-        qs.append("PREFIX foaf: <http://xmlns.com/foaf/0.1/>");
-        qs.append("PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>");
-        qs.append("PREFIX dbpprop: <http://dbpedia.org/property/>");
-        qs.append("PREFIX dbo: <http://dbpedia.org>");
-        qs.append("SELECT ?album ?albumName ?Artist ?abstract ");
-        qs.append("{\n");
-        qs.append("?album a dbpedia-owl:Album .");
-        qs.append("?album dbo:abstract ?abstract .");
-        qs.append("?album rdfs:label ?albumName .");
-        qs.append("?album dbpedia-owl:artist ?Artist .");
-        qs.append("FILTER regex(");
-        qs.append("?album, 'Mezmerize'");
-        qs.append(")");
-        qs.append("}");
-        qs.append("LIMIT 100");
+        +"PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n"
+        +"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n"
+        +"PREFIX dbpedia-owl: <http://dbpedia.org/ontology/>\n"
+        +"PREFIX dbpprop: <http://dbpedia.org/property/>\n\n"
+        +"SELECT ?album ?albumName ?Artist ?abstract \n"
+        +"WHERE{\n"
+        +"?album a dbpedia-owl:Album .\n"
+        +"?album dbo:abstract ?abstract .\n"
+        +"?album rdfs:label ?albumName .\n"
+        +"?album dbpedia-owl:artist ?Artist .\n"
+        +"FILTER regex(\n"
+        +"?album, 'Mezmerize'\n"
+        +")\n"
+        +"}\n"
+        +"LIMIT 100");
+
+        //System.out.println(qs);
 
         QueryEngineHTTP qe = new QueryEngineHTTP("http://dbpedia.org/sparql", qs.toString());
-        String output = ResultSetFormatter.asText(qe.execSelect());
-        System.out.println(output);
+        String output = ResultSetFormatter.asXMLString(qe.execSelect());
+        //System.out.println(output);
         qe.close();
         
         return output;
     }
 
-    public String toXML(){
-        String XML = new String();
-        return XML;
+    public static String XMLtoJSON(String XMLString){
+        int ppif = 4;
+        JSONObject xmlJSONObj = XML.toJSONObject(XMLString);
+        String jsonString = xmlJSONObj.toString(ppif);
+        System.out.println(jsonString);
+        return jsonString;
     }
-
-    public String toHTML(){
-        String HTML = new String();
-        return HTML;
-    }
-
 }
